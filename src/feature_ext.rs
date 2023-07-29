814422e6ec5178e6d62eb04ef655e97fe99187fa
Laurențiu Nicola <lnicola@dend.ro>
2023-07-29 19:47:49 +0300

    Add initial sample-extraction version

---
+++ src/feature_ext.rs
@@ -0,0 +1,61 @@
+use std::ffi::{c_double, c_int, c_longlong};
+
+use gdal::vector::{Feature, FieldValue};
+
+pub trait FeatureExt {
+    fn set_field_double_by_index(&self, idx: usize, value: f64);
+    fn set_field_integer_by_index(&self, idx: usize, value: i32);
+    fn set_field_integer64_by_index(&self, idx: usize, value: i64);
+    fn set_field_by_index(&self, idx: usize, value: &FieldValue);
+}
+
+impl FeatureExt for Feature<'_> {
+    fn set_field_double_by_index(&self, idx: usize, value: f64) {
+        unsafe {
+            gdal_sys::OGR_F_SetFieldDouble(self.c_feature(), idx as i32, value as c_double);
+        }
+    }
+
+    fn set_field_integer_by_index(&self, idx: usize, value: i32) {
+        unsafe {
+            gdal_sys::OGR_F_SetFieldInteger(self.c_feature(), idx as i32, value as c_int);
+        }
+    }
+
+    fn set_field_integer64_by_index(&self, idx: usize, value: i64) {
+        unsafe {
+            gdal_sys::OGR_F_SetFieldInteger64(self.c_feature(), idx as i32, value as c_longlong);
+        }
+    }
+
+    fn set_field_by_index(&self, idx: usize, value: &FieldValue) {
+        match value {
+            FieldValue::IntegerValue(value) => self.set_field_integer_by_index(idx, *value),
+            // FieldValue::IntegerListValue(value) => self.set_field_integer_list(field_name, value),
+            FieldValue::Integer64Value(value) => self.set_field_integer64_by_index(idx, *value),
+            // FieldValue::Integer64ListValue(value) => {
+            //     self.set_field_integer64_list(field_name, value)
+            // }
+            // FieldValue::StringValue(ref value) => self.set_field_string(field_name, value.as_str()),
+            // FieldValue::StringListValue(ref value) => {
+            //     let strs = value.iter().map(String::as_str).collect::<Vec<&str>>();
+            //     self.set_field_string_list(field_name, &strs)
+            // }
+            FieldValue::RealValue(value) => self.set_field_double_by_index(idx, *value),
+            // FieldValue::RealListValue(value) => self.set_field_double_list(field_name, value),
+            // FieldValue::DateValue(value) => {
+            //     let dv = value
+            //         .and_hms_opt(0, 0, 0)
+            //         .ok_or_else(|| GdalError::DateError("offset to midnight".into()))?;
+            //     let dt = DateTime::from_utc(
+            //         dv,
+            //         FixedOffset::east_opt(0)
+            //             .ok_or_else(|| GdalError::DateError("utc offset".into()))?,
+            //     );
+            //     self.set_field_datetime(field_name, dt)
+            // }
+            // FieldValue::DateTimeValue(value) => self.set_field_datetime(field_name, *value),
+            _ => unimplemented!(),
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,207 @@
+mod feature_ext;
+
+use std::{collections::HashMap, sync::mpsc, thread};
+
+use anyhow::Result;
+use gdal::{
+    raster::GdalDataType,
+    spatial_ref::SpatialRef,
+    vector::{
+        Feature, FieldDefn, FieldValue, Geometry, LayerAccess, OGRFieldType, OGRwkbGeometryType,
+    },
+    Dataset, DriverManager, GeoTransformEx, LayerOptions, Metadata,
+};
+
+use crate::feature_ext::FeatureExt;
+
+#[derive(Clone, Copy)]
+enum BandValue {
+    U16(u16),
+    I16(i16),
+    F32(f32),
+}
+
+struct FieldDefinition {
+    name: String,
+    field_type: OGRFieldType::Type,
+    width: Option<i32>,
+    precision: Option<i32>,
+}
+
+impl FieldDefinition {
+    fn to_field_defn(&self) -> gdal::errors::Result<FieldDefn> {
+        let field_defn = FieldDefn::new(&self.name, self.field_type)?;
+        if let Some(width) = self.width {
+            field_defn.set_width(width);
+        }
+        if let Some(precision) = self.precision {
+            field_defn.set_precision(precision);
+        }
+        Ok(field_defn)
+    }
+}
+
+fn main() -> Result<()> {
+    let args = std::env::args().collect::<Vec<_>>();
+    let image = Dataset::open(&args[1])?;
+    let geo_transform = image.geo_transform()?;
+    let geo_transform = geo_transform.invert()?;
+    let block_size = image.rasterband(1)?.block_size();
+    dbg!(block_size);
+
+    let band_count = image.raster_count() as usize;
+    for band_idx in 0..band_count {
+        let band = image.rasterband(band_idx as isize + 1)?;
+        assert!(band.block_size() == block_size);
+    }
+    let mut tile_points = HashMap::<_, Vec<_>>::new();
+    let point_ds = Dataset::open(&args[2])?;
+    let mut layer = point_ds.layer(0)?;
+    dbg!(layer.feature_count());
+    for feature in layer.features() {
+        let geometry = feature.geometry().unwrap().clone();
+        let (x, y, _) = geometry.get_point(0);
+        let (x, y) = geo_transform.apply(x, y);
+        let (block_x, block_y) = (
+            (x / block_size.0 as f64) as usize,
+            (y / block_size.1 as f64) as usize,
+        );
+        let (x, y) = (x as usize, y as usize);
+        tile_points.entry((block_x, block_y)).or_default().push((
+            x % block_size.0,
+            y % block_size.1,
+            geometry,
+            feature.fields().map(|f| f.1).collect::<Vec<_>>(),
+        ));
+    }
+    let spatial_ref = layer.spatial_ref().map(|sr| sr.to_wkt()).transpose()?;
+    let mut output_fields = Vec::new();
+    for field in layer.defn().fields() {
+        let field_definition = FieldDefinition {
+            name: field.name(),
+            field_type: field.field_type(),
+            width: Some(field.width()),
+            precision: Some(field.precision()),
+        };
+        output_fields.push(field_definition);
+    }
+    for band_idx in 1..=band_count {
+        let band = image.rasterband(band_idx as isize)?;
+        let name = match band.description() {
+            Ok(name) if !name.is_empty() => name,
+            _ => format!("band_{band_idx}"),
+        };
+        let field_type = match band.band_type() {
+            GdalDataType::UInt16 => OGRFieldType::OFTInteger,
+            GdalDataType::Int16 => OGRFieldType::OFTInteger,
+            GdalDataType::Float32 => OGRFieldType::OFTReal,
+            _ => unimplemented!(),
+        };
+        let field_definition = FieldDefinition {
+            name,
+            field_type,
+            width: None,
+            precision: None,
+        };
+        output_fields.push(field_definition);
+    }
+
+    let (tx, rx) =
+        mpsc::sync_channel::<(Vec<BandValue>, Vec<(Vec<u8>, Vec<Option<FieldValue>>)>)>(128);
+    let output_thread = thread::spawn(move || {
+        let mut output =
+            DriverManager::get_driver_by_name("GPKG")?.create_vector_only("output.gpkg")?;
+        let output_layer = output.create_layer(LayerOptions {
+            name: "output",
+            srs: spatial_ref
+                .map(|wkt| SpatialRef::from_wkt(&wkt))
+                .transpose()?
+                .as_ref(),
+            ty: OGRwkbGeometryType::wkbPoint,
+            options: None,
+        })?;
+        for field in output_fields {
+            let field_defn = field.to_field_defn()?;
+            field_defn.add_to_layer(&output_layer)?;
+        }
+        for (sample_values, points) in rx {
+            let tx = output.start_transaction()?;
+            let output_layer = tx.layer(0)?;
+            for (idx, (wkb, fields)) in points.into_iter().enumerate() {
+                let mut feature = Feature::new(output_layer.defn())?;
+                let field_offset = fields.len();
+                for (field_idx, field_value) in fields.into_iter().enumerate() {
+                    if let Some(field_value) = field_value {
+                        feature.set_field_by_index(field_idx, &field_value);
+                    }
+                }
+                for band_idx in 0..band_count {
+                    match sample_values[band_count * idx + band_idx] {
+                        BandValue::U16(value) => {
+                            feature
+                                .set_field_integer_by_index(band_idx + field_offset, value as i32);
+                        }
+                        BandValue::I16(value) => {
+                            feature
+                                .set_field_integer_by_index(band_idx + field_offset, value as i32);
+                        }
+                        BandValue::F32(value) => {
+                            feature
+                                .set_field_double_by_index(band_idx + field_offset, value as f64);
+                        }
+                    }
+                }
+                let geometry = Geometry::from_wkb(&wkb)?;
+                feature.set_geometry(geometry)?;
+                feature.create(&output_layer)?;
+            }
+            tx.commit()?;
+        }
+        Ok::<_, gdal::errors::GdalError>(())
+    });
+
+    let mut tile_points = tile_points.into_iter().collect::<Vec<_>>();
+    tile_points.sort_by_key(|t| (t.0 .1, t.0 .0));
+
+    for ((block_x, block_y), points) in tile_points {
+        println!("{block_x} {block_y}");
+        let mut sample_values = vec![BandValue::I16(0); points.len() * band_count];
+        for band_idx in 0..band_count {
+            let band = image.rasterband(band_idx as isize + 1)?;
+            match band.band_type() {
+                GdalDataType::UInt16 => {
+                    let buf = band.read_block::<u16>((block_x, block_y))?;
+                    for (idx, &(bx, by, ..)) in points.iter().enumerate() {
+                        let pix = buf[(by, bx)];
+                        sample_values[band_count * idx + band_idx] = BandValue::U16(pix);
+                    }
+                }
+                GdalDataType::Int16 => {
+                    let buf = band.read_block::<i16>((block_x, block_y))?;
+                    for (idx, &(bx, by, ..)) in points.iter().enumerate() {
+                        let pix = buf[(by, bx)];
+                        sample_values[band_count * idx + band_idx] = BandValue::I16(pix);
+                    }
+                }
+                GdalDataType::Float32 => {
+                    let buf = band.read_block::<f32>((block_x, block_y))?;
+                    for (idx, &(bx, by, ..)) in points.iter().enumerate() {
+                        let pix = buf[(by, bx)];
+                        sample_values[band_count * idx + band_idx] = BandValue::F32(pix);
+                    }
+                }
+                _ => unimplemented!(),
+            }
+        }
+        let field_values = points
+            .into_iter()
+            .map(|p| -> Result<(Vec<u8>, Vec<Option<FieldValue>>)> { Ok((p.2.wkb()?, p.3)) })
+            .collect::<Result<Vec<_>>>()?;
+        tx.send((sample_values, field_values))?;
+    }
+
+    drop(tx);
+    output_thread.join().unwrap()?;
+
+    Ok(())
+}
