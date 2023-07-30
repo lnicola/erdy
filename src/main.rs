9f04b8d0336889e8ea079da7411256e9f0a8d2cb
Laurențiu Nicola <lnicola@dend.ro>
2023-07-30 16:58:02 +0300

    Move out block reader

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 mod gdal_ext;
+mod threaded_block_reader;
 
 use std::{
     collections::HashMap,
     mem,
     path::PathBuf,
-    sync::mpsc::{self, Sender, SyncSender},
-    thread::{self, JoinHandle},
+    sync::mpsc::{self, SyncSender},
+    thread,
 };
 
 use anyhow::Result;
@@ -17,8 +18,12 @@ use gdal::{
     },
     Dataset, DriverManager, GeoTransformEx, LayerOptions, Metadata,
 };
+use threaded_block_reader::{BlockFinalizer, BlockReducer};
 
-use crate::gdal_ext::{FeatureExt, RasterBandExt, TypedBlock};
+use crate::{
+    gdal_ext::{FeatureExt, TypedBlock},
+    threaded_block_reader::ThreadedBlockReader,
+};
 
 #[derive(Clone, Copy)]
 enum BandValue {
@@ -45,86 +50,6 @@ impl FieldDefinition {
         }
         Ok(field_defn)
     }
-}
-
-struct ThreadedBlockReader<T> {
-    workers: Vec<JoinHandle<()>>,
-    request_txs: Vec<Sender<(usize, usize, T)>>,
-    current_worker: usize,
-}
-
-impl<T: Send + 'static> ThreadedBlockReader<T> {
-    fn new<R: BlockReducer<InputState = T>, F: BlockFinalizer<Input = R::Output>>(
-        path: PathBuf,
-        block_finalizer: F,
-    ) -> Self {
-        let num_threads = 8;
-
-        let mut workers = Vec::with_capacity(num_threads);
-        let mut request_txs = Vec::with_capacity(num_threads);
-        for _ in 0..num_threads {
-            let (tx, rx) = mpsc::channel();
-            let path = path.clone();
-            let block_finalizer = block_finalizer.clone();
-
-            let worker = thread::spawn(move || {
-                let dataset = Dataset::open(path).unwrap();
-                let band_count = dataset.raster_count() as usize;
-
-                for (x, y, data) in rx {
-                    let mut block_reducer = R::new(band_count, data);
-
-                    for band_index in 0..band_count {
-                        let band = dataset.rasterband(band_index as isize + 1).unwrap();
-                        let block = band.read_typed_block(x, y).unwrap();
-                        block_reducer.push_block(band_index, band_count, block);
-                    }
-
-                    println!("{x} {y}");
-                    block_finalizer.apply(block_reducer.finalize());
-                }
-            });
-            workers.push(worker);
-            request_txs.push(tx);
-        }
-
-        Self {
-            workers,
-            request_txs,
-            current_worker: 0,
-        }
-    }
-
-    fn submit(&mut self, x: usize, y: usize, data: T) {
-        self.request_txs[self.current_worker]
-            .send((x, y, data))
-            .unwrap();
-        self.current_worker += 1;
-        if self.current_worker == self.request_txs.len() {
-            self.current_worker = 0;
-        }
-    }
-
-    fn join(self) {
-        for worker in self.workers {
-            let _ = worker.join();
-        }
-    }
-}
-
-trait BlockReducer {
-    type InputState;
-    type Output: Send + 'static;
-
-    fn new(band_count: usize, input_state: Self::InputState) -> Self;
-    fn push_block(&mut self, band_index: usize, band_count: usize, block: TypedBlock);
-    fn finalize(self) -> Self::Output;
-}
-
-trait BlockFinalizer: Clone + Send + 'static {
-    type Input: Send;
-
-    fn apply(&self, input: Self::Input);
 }
 
 struct SamplingBlockReducer {
---
+++ src/threaded_block_reader.rs
@@ -0,0 +1,89 @@
+use std::{
+    path::PathBuf,
+    sync::mpsc::{self, Sender},
+    thread::{self, JoinHandle},
+};
+
+use gdal::Dataset;
+
+use crate::gdal_ext::{RasterBandExt, TypedBlock};
+
+pub trait BlockReducer {
+    type InputState;
+    type Output: Send + 'static;
+
+    fn new(band_count: usize, input_state: Self::InputState) -> Self;
+    fn push_block(&mut self, band_index: usize, band_count: usize, block: TypedBlock);
+    fn finalize(self) -> Self::Output;
+}
+
+pub trait BlockFinalizer: Clone + Send + 'static {
+    type Input: Send;
+
+    fn apply(&self, input: Self::Input);
+}
+
+pub struct ThreadedBlockReader<T> {
+    workers: Vec<JoinHandle<()>>,
+    request_txs: Vec<Sender<(usize, usize, T)>>,
+    current_worker: usize,
+}
+
+impl<T: Send + 'static> ThreadedBlockReader<T> {
+    pub fn new<R: BlockReducer<InputState = T>, F: BlockFinalizer<Input = R::Output>>(
+        path: PathBuf,
+        block_finalizer: F,
+    ) -> Self {
+        let num_threads = 8;
+
+        let mut workers = Vec::with_capacity(num_threads);
+        let mut request_txs = Vec::with_capacity(num_threads);
+        for _ in 0..num_threads {
+            let (tx, rx) = mpsc::channel();
+            let path = path.clone();
+            let block_finalizer = block_finalizer.clone();
+
+            let worker = thread::spawn(move || {
+                let dataset = Dataset::open(path).unwrap();
+                let band_count = dataset.raster_count() as usize;
+
+                for (x, y, data) in rx {
+                    let mut block_reducer = R::new(band_count, data);
+
+                    for band_index in 0..band_count {
+                        let band = dataset.rasterband(band_index as isize + 1).unwrap();
+                        let block = band.read_typed_block(x, y).unwrap();
+                        block_reducer.push_block(band_index, band_count, block);
+                    }
+
+                    println!("{x} {y}");
+                    block_finalizer.apply(block_reducer.finalize());
+                }
+            });
+            workers.push(worker);
+            request_txs.push(tx);
+        }
+
+        Self {
+            workers,
+            request_txs,
+            current_worker: 0,
+        }
+    }
+
+    pub fn submit(&mut self, x: usize, y: usize, data: T) {
+        self.request_txs[self.current_worker]
+            .send((x, y, data))
+            .unwrap();
+        self.current_worker += 1;
+        if self.current_worker == self.request_txs.len() {
+            self.current_worker = 0;
+        }
+    }
+
+    pub fn join(self) {
+        for worker in self.workers {
+            let _ = worker.join();
+        }
+    }
+}
