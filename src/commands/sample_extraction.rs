88c4a85f4f11c474ffa21474f363fe222afd38c3
Laurențiu Nicola <lnicola@dend.ro>
2023-07-31 17:27:22 +0300

    Add support for extracting multiple sets of points

--- src/commands/sample_extraction.rs
+++ src/commands/sample_extraction.rs
@@ -29,10 +29,12 @@ pub struct SampleExtractionArgs {
     input: PathBuf,
 
     /// Sampling positions
-    points: PathBuf,
+    #[arg(long, value_parser, num_args = 1..)]
+    points: Vec<PathBuf>,
 
-    /// Output dataset
-    output: PathBuf,
+    /// Output datasets
+    #[arg(long, value_parser, num_args = 1..)]
+    outputs: Vec<PathBuf>,
 
     /// Output format
     #[arg(short, long)]
@@ -54,6 +56,7 @@ enum BandValue {
     F32(f32),
 }
 
+#[derive(Clone)]
 struct FieldDefinition {
     name: String,
     field_type: OGRFieldType::Type,
@@ -118,17 +121,32 @@ impl BlockReducer for SamplingBlockReducer {
 }
 
 #[derive(Clone)]
-struct BlockSender(SyncSender<(Vec<BandValue>, Vec<SamplingPoint>)>);
+struct BlockSender(Vec<SyncSender<(Vec<BandValue>, Vec<SamplingPoint>)>>);
 
 impl BlockFinalizer for BlockSender {
     type Input = (Vec<BandValue>, Vec<SamplingPoint>);
 
     fn apply(&self, input: Self::Input) {
-        self.0.send(input).unwrap()
+        let band_count = input.0.len() / input.1.len();
+        let mut layers = HashMap::<_, (Vec<BandValue>, Vec<SamplingPoint>)>::new();
+        for (idx, point) in input.1.into_iter().enumerate() {
+            {
+                let entry = layers.entry(point.input_index).or_default();
+                entry
+                    .0
+                    .extend_from_slice(&input.0[idx * band_count..(idx + 1) * band_count]);
+                entry.1.push(point);
+            }
+        }
+
+        for (input_idx, data) in layers {
+            self.0[input_idx].send(data).unwrap()
+        }
     }
 }
 
 struct SamplingPoint {
+    input_index: usize,
     _fid: Option<u64>,
     bx: usize,
     by: usize,
@@ -149,48 +167,14 @@ impl SampleExtractionArgs {
             let band = image.rasterband(band_idx as isize + 1)?;
             assert_eq!(band.block_size(), block_size);
         }
-        let mut tile_points = HashMap::<_, Vec<_>>::new();
-        let point_ds = Dataset::open(&self.points)?;
-        let mut layer = point_ds.layer(0)?;
-        let layer_name = layer.name();
 
         if let Some(fields) = self.fields.as_ref() {
             assert_eq!(fields.len(), band_count);
         }
 
-        dbg!(layer.feature_count());
-        for feature in layer.features() {
-            let (orig_x, orig_y, _) = feature.geometry().as_ref().unwrap().get_point(0);
-            let (x, y) = geo_transform.apply(orig_x, orig_y);
-            let (block_x, block_y) = (
-                (x / block_size.0 as f64) as usize,
-                (y / block_size.1 as f64) as usize,
-            );
-            let (x, y) = (x as usize, y as usize);
-            let sampling_point = SamplingPoint {
-                _fid: feature.fid(),
-                bx: x % block_size.0,
-                by: y % block_size.1,
-                orig_x,
-                orig_y,
-                original_fields: feature.fields().map(|f| f.1).collect::<Vec<_>>(),
-            };
-            tile_points
-                .entry((block_x, block_y))
-                .or_default()
-                .push(sampling_point);
-        }
-        let spatial_ref = layer.spatial_ref().map(|sr| sr.to_wkt()).transpose()?;
-        let mut output_fields = Vec::new();
-        for field in layer.defn().fields() {
-            let field_definition = FieldDefinition {
-                name: field.name(),
-                field_type: field.field_type(),
-                width: Some(field.width()),
-                precision: Some(field.precision()),
-            };
-            output_fields.push(field_definition);
-        }
+        assert_eq!(self.points.len(), self.outputs.len());
+
+        let mut band_fields = Vec::with_capacity(band_count);
         for band_index in 1..=band_count {
             let band = image.rasterband(band_index as isize)?;
             let name = match self.fields.as_ref() {
@@ -213,81 +197,149 @@ impl SampleExtractionArgs {
                 width: None,
                 precision: None,
             };
-            output_fields.push(field_definition);
+            band_fields.push(field_definition);
         }
 
-        let (tx, rx) = mpsc::sync_channel::<(Vec<BandValue>, Vec<SamplingPoint>)>(128);
+        let mut tile_points = HashMap::<_, Vec<_>>::new();
+        let mut layer_names = Vec::with_capacity(self.points.len());
+        let mut spatial_refs = Vec::with_capacity(self.points.len());
+        let mut output_fields = Vec::with_capacity(self.points.len());
+        for (idx, path) in self.points.iter().enumerate() {
+            let ds = Dataset::open(path)?;
+            let mut layer = ds.layer(0)?;
+
+            let layer_name = layer.name();
+            layer_names.push(layer_name);
+
+            dbg!(layer.feature_count());
+            for feature in layer.features() {
+                let (orig_x, orig_y, _) = feature.geometry().as_ref().unwrap().get_point(0);
+                let (x, y) = geo_transform.apply(orig_x, orig_y);
+                let (block_x, block_y) = (
+                    (x / block_size.0 as f64) as usize,
+                    (y / block_size.1 as f64) as usize,
+                );
+                let (x, y) = (x as usize, y as usize);
+                let sampling_point = SamplingPoint {
+                    input_index: idx,
+                    _fid: feature.fid(),
+                    bx: x % block_size.0,
+                    by: y % block_size.1,
+                    orig_x,
+                    orig_y,
+                    original_fields: feature.fields().map(|f| f.1).collect::<Vec<_>>(),
+                };
+                tile_points
+                    .entry((block_x, block_y))
+                    .or_default()
+                    .push(sampling_point);
+            }
+
+            let spatial_ref = layer.spatial_ref().map(|sr| sr.to_wkt()).transpose()?;
+            spatial_refs.push(spatial_ref);
+
+            let mut layer_fields = Vec::new();
+            for field in layer.defn().fields() {
+                let field_definition = FieldDefinition {
+                    name: field.name(),
+                    field_type: field.field_type(),
+                    width: Some(field.width()),
+                    precision: Some(field.precision()),
+                };
+                layer_fields.push(field_definition);
+            }
+            layer_fields.extend_from_slice(&band_fields);
+
+            output_fields.push(layer_fields);
+        }
+
+        let mut output_txs = Vec::with_capacity(self.points.len());
         thread::scope(|scope| -> Result<()> {
-            let output_thread = scope.spawn(move || {
-                let mut output = DriverManager::get_driver_by_name(&self.format)?
-                    .create_vector_only(&self.output)?;
-                let output_layer = output.create_layer(LayerOptions {
-                    name: &layer_name,
-                    srs: spatial_ref
-                        .map(|wkt| SpatialRef::from_wkt(&wkt))
-                        .transpose()?
-                        .as_ref(),
-                    ty: OGRwkbGeometryType::wkbPoint,
-                    options: None,
-                })?;
-                for field in output_fields {
-                    let field_defn = field.to_field_defn()?;
-                    field_defn.add_to_layer(&output_layer)?;
-                }
-                for (sample_values, mut points) in rx {
-                    let tx = output.start_transaction()?;
-                    let output_layer = tx.layer(0)?;
-                    points.sort_by_key(|p| p._fid);
-                    for (idx, point) in points.into_iter().enumerate() {
-                        let mut feature = Feature::new(output_layer.defn())?;
-                        // this is too slow, don't do it by default
-                        // feature.set_fid(point._fid)?;
-                        let field_offset = point.original_fields.len();
-                        for (field_idx, field_value) in
-                            point.original_fields.into_iter().enumerate()
-                        {
-                            if let Some(field_value) = field_value {
-                                feature.set_field_by_index(field_idx, &field_value);
-                            }
-                        }
-                        for band_idx in 0..band_count {
-                            match sample_values[band_count * idx + band_idx] {
-                                BandValue::U16(value) => {
-                                    feature.set_field_integer_by_index(
-                                        band_idx + field_offset,
-                                        value as i32,
-                                    );
-                                }
-                                BandValue::I16(value) => {
-                                    feature.set_field_integer_by_index(
-                                        band_idx + field_offset,
-                                        value as i32,
-                                    );
-                                }
-                                BandValue::F32(value) => {
-                                    feature.set_field_double_by_index(
-                                        band_idx + field_offset,
-                                        value as f64,
-                                    );
-                                }
-                            }
-                        }
-                        let mut geometry = Geometry::empty(OGRwkbGeometryType::wkbPoint)?;
-                        geometry.add_point_2d((point.orig_x, point.orig_y));
-                        feature.set_geometry(geometry)?;
-                        feature.create(&output_layer)?;
+            let mut output_threads = Vec::with_capacity(self.points.len());
+            for (path, layer_name, spatial_ref, layer_fields) in self
+                .outputs
+                .iter()
+                .zip(layer_names)
+                .zip(spatial_refs)
+                .zip(output_fields)
+                .map(|(((a, b), c), d)| (a, b, c, d))
+            {
+                let (tx, rx) = mpsc::sync_channel::<(Vec<BandValue>, Vec<SamplingPoint>)>(128);
+                output_txs.push(tx);
+
+                let thread = scope.spawn(move || {
+                    let mut output = DriverManager::get_driver_by_name(&self.format)?
+                        .create_vector_only(path)?;
+                    let output_layer = output.create_layer(LayerOptions {
+                        name: &layer_name,
+                        srs: spatial_ref
+                            .map(|wkt| SpatialRef::from_wkt(&wkt))
+                            .transpose()?
+                            .as_ref(),
+                        ty: OGRwkbGeometryType::wkbPoint,
+                        options: None,
+                    })?;
+                    for field in layer_fields {
+                        let field_defn = field.to_field_defn()?;
+                        field_defn.add_to_layer(&output_layer)?;
                     }
-                    tx.commit()?;
-                }
-                output.close()?;
-                mem::forget(output);
-                Ok::<_, gdal::errors::GdalError>(())
-            });
+                    for (sample_values, mut points) in rx {
+                        let tx = output.start_transaction()?;
+                        let output_layer = tx.layer(0)?;
+                        points.sort_by_key(|p| p._fid);
+                        for (idx, point) in points.into_iter().enumerate() {
+                            let mut feature = Feature::new(output_layer.defn())?;
+                            // this is too slow, don't do it by default
+                            #[cfg(FALSE)]
+                            feature.set_fid(point._fid)?;
+                            let field_offset = point.original_fields.len();
+                            for (field_idx, field_value) in
+                                point.original_fields.into_iter().enumerate()
+                            {
+                                if let Some(field_value) = field_value {
+                                    feature.set_field_by_index(field_idx, &field_value);
+                                }
+                            }
+                            for band_idx in 0..band_count {
+                                match sample_values[band_count * idx + band_idx] {
+                                    BandValue::U16(value) => {
+                                        feature.set_field_integer_by_index(
+                                            band_idx + field_offset,
+                                            value as i32,
+                                        );
+                                    }
+                                    BandValue::I16(value) => {
+                                        feature.set_field_integer_by_index(
+                                            band_idx + field_offset,
+                                            value as i32,
+                                        );
+                                    }
+                                    BandValue::F32(value) => {
+                                        feature.set_field_double_by_index(
+                                            band_idx + field_offset,
+                                            value as f64,
+                                        );
+                                    }
+                                }
+                            }
+                            let mut geometry = Geometry::empty(OGRwkbGeometryType::wkbPoint)?;
+                            geometry.add_point_2d((point.orig_x, point.orig_y));
+                            feature.set_geometry(geometry)?;
+                            feature.create(&output_layer)?;
+                        }
+                        tx.commit()?;
+                    }
+                    output.close()?;
+                    mem::forget(output);
+                    Ok::<_, gdal::errors::GdalError>(())
+                });
+                output_threads.push(thread);
+            }
 
             let mut tile_points = tile_points.into_iter().collect::<Vec<_>>();
             tile_points.sort_by_key(|t| (t.0 .1, t.0 .0));
 
-            let block_sender = BlockSender(tx);
+            let block_sender = BlockSender(output_txs);
 
             let num_threads = NonZeroUsize::new(self.num_threads.unwrap_or(8))
                 .unwrap()
@@ -303,7 +355,9 @@ impl SampleExtractionArgs {
             }
             drop(block_reader);
 
-            output_thread.join().unwrap()?;
+            for thread in output_threads {
+                thread.join().unwrap()?;
+            }
             Ok(())
         })
     }
