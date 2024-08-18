1833396a1e431271b8661c5e5851d46f0bd0d6d5
Laurențiu Nicola <lnicola@dend.ro>
2024-08-18 13:54:40 +0300

    Support more field types

--- src/gdal_ext/feature_ext.rs
+++ src/gdal_ext/feature_ext.rs
@@ -8,10 +8,13 @@ use gdal_sys::OGRErr;
 
 pub trait FeatureExt {
     fn set_fid(&mut self, fid: Option<u64>) -> gdal::errors::Result<()>;
-    fn set_field_double_by_index(&mut self, idx: usize, value: f64);
     fn set_field_integer_by_index(&mut self, idx: usize, value: i32);
     fn set_field_integer_list_by_index(&mut self, idx: usize, value: &[i32]);
     fn set_field_integer64_by_index(&mut self, idx: usize, value: i64);
+    fn set_field_integer64_list_by_index(&mut self, idx: usize, value: &[i64]);
+    // fn set_field_string_by_index(&mut self, idx: usize, value:);
+    fn set_field_double_by_index(&mut self, idx: usize, value: f64);
+    fn set_field_double_list_by_index(&mut self, idx: usize, value: &[f64]);
     fn set_field_by_index(&mut self, idx: usize, value: &FieldValue);
 }
 
@@ -29,12 +32,6 @@ impl FeatureExt for Feature<'_> {
         }
 
         Ok(())
-    }
-
-    fn set_field_double_by_index(&mut self, idx: usize, value: f64) {
-        unsafe {
-            gdal_sys::OGR_F_SetFieldDouble(self.c_feature(), idx as i32, value as c_double);
-        }
     }
 
     fn set_field_integer_by_index(&mut self, idx: usize, value: i32) {
@@ -60,21 +57,49 @@ impl FeatureExt for Feature<'_> {
         }
     }
 
+    fn set_field_integer64_list_by_index(&mut self, idx: usize, value: &[i64]) {
+        unsafe {
+            gdal_sys::OGR_F_SetFieldInteger64List(
+                self.c_feature(),
+                idx as i32,
+                value.len() as c_int,
+                value.as_ptr(),
+            )
+        };
+    }
+
+    fn set_field_double_by_index(&mut self, idx: usize, value: f64) {
+        unsafe {
+            gdal_sys::OGR_F_SetFieldDouble(self.c_feature(), idx as i32, value as c_double);
+        }
+    }
+
+    fn set_field_double_list_by_index(&mut self, idx: usize, value: &[f64]) {
+        unsafe {
+            gdal_sys::OGR_F_SetFieldDoubleList(
+                self.c_feature(),
+                idx as i32,
+                value.len() as c_int,
+                value.as_ptr(),
+            )
+        };
+    }
+
     fn set_field_by_index(&mut self, idx: usize, value: &FieldValue) {
         match value {
             FieldValue::IntegerValue(value) => self.set_field_integer_by_index(idx, *value),
             FieldValue::IntegerListValue(value) => self.set_field_integer_list_by_index(idx, value),
             FieldValue::Integer64Value(value) => self.set_field_integer64_by_index(idx, *value),
-            // FieldValue::Integer64ListValue(value) => {
-            //     self.set_field_integer64_list(idx, value)
-            // }
+            FieldValue::Integer64ListValue(value) => {
+                self.set_field_integer64_list_by_index(idx, value)
+            }
             // FieldValue::StringValue(ref value) => self.set_field_string(idx, value.as_str()),
             // FieldValue::StringListValue(ref value) => {
             //     let strs = value.iter().map(String::as_str).collect::<Vec<&str>>();
             //     self.set_field_string_list(idx, &strs)
             // }
             FieldValue::RealValue(value) => self.set_field_double_by_index(idx, *value),
-            // FieldValue::RealListValue(value) => self.set_field_double_list(idx, value),
+            FieldValue::RealListValue(value) => self.set_field_double_list_by_index(idx, value),
             // FieldValue::DateValue(value) => {
             //     let dv = value
             //         .and_hms_opt(0, 0, 0)
